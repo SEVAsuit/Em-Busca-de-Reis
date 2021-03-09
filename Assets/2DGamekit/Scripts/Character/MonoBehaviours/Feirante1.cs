0b421896b2a2e40dea29f1577eff625e70ee8de6
Dutyword <39968021+Dutyword@users.noreply.github.com>
2021-03-09 06:16:17 -0300

    Adicionado o projeto

---
+++ Assets/2DGamekit/Scripts/Character/MonoBehaviours/Feirante1.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Gamekit2D
+{
+    [RequireComponent(typeof(CharacterController2D))]
+    [RequireComponent(typeof(Collider2D))]
+    public class Feirante1 : EnemyBehaviour
+    {
+        public new void Shooting()
+        {
+            Vector2 shootPosition = shootingOrigin.transform.localPosition;
+
+            //if we are flipped compared to normal, we need to localy flip the shootposition too
+            if ((spriteFaceLeft && m_SpriteForward.x > 0) || (!spriteFaceLeft && m_SpriteForward.x > 0))
+                shootPosition.x *= -1;
+
+            BulletObject obj = m_BulletPool.Pop(shootingOrigin.TransformPoint(shootPosition));
+			
+			BulletObject obj2 = m_BulletPool.Pop(shootingOrigin.TransformPoint(shootPosition));
+			
+			BulletObject obj3 = m_BulletPool.Pop(shootingOrigin.TransformPoint(shootPosition));
+
+            shootingAudio.PlayRandomSound();
+			
+			float x=(m_TargetShootPosition-shootingOrigin.transform.position).x;
+			
+			Vector3 vectorHigh = new Vector3(x,10,0);
+			Vector3 vectorMedium = new Vector3(x,5,0);
+			Vector3 vectorLow = new Vector3(x,2,0);
+			
+			obj.rigidbody2D.velocity = vectorHigh;
+			obj2.rigidbody2D.velocity = vectorMedium;
+			obj3.rigidbody2D.velocity = vectorLow;
+        }
+
+        //This will give the velocity vector needed to give to the bullet rigidbody so it reach the given target from the origin.
+        private Vector3 GetProjectilVelocity(Vector3 target, Vector3 origin)
+        {
+            const float projectileSpeed = 7.5f;
+
+            Vector3 velocity = Vector3.zero;
+            Vector3 toTarget = target - origin;
+
+            float gSquared = Physics.gravity.sqrMagnitude;
+            float b = projectileSpeed * projectileSpeed + Vector3.Dot(toTarget, Physics.gravity);
+            float discriminant = b * b - gSquared * toTarget.sqrMagnitude;
+
+           /* // Check whether the target is reachable at max speed or less.
+            if (discriminant < 0)
+            {
+                velocity = toTarget;
+                velocity.y = 0;
+                velocity.Normalize();
+                velocity.y = 0.7f;
+
+                velocity *= projectileSpeed;
+                return velocity;
+            }*/
+
+            float discRoot = Mathf.Sqrt(discriminant);
+
+            // Highest
+            float T_max = Mathf.Sqrt((b + discRoot) * 2f / gSquared);
+
+            // Lowest speed arc
+            float T_lowEnergy = Mathf.Sqrt(Mathf.Sqrt(toTarget.sqrMagnitude * 4f / gSquared));
+
+            // Most direct with max speed
+            float T_min = Mathf.Sqrt((b - discRoot) * 2f / gSquared);
+
+            float T = 0;
+
+            // 0 = highest, 1 = lowest, 2 = most direct
+            int shotType = 1;
+
+            switch (shotType)
+            {
+                case 0:
+                    T = T_max;
+                    break;
+                case 1:
+                    T = T_lowEnergy;
+                    break;
+                case 2:
+                    T = T_min;
+                    break;
+                default:
+                    break;
+            }
+
+            velocity = toTarget / T - Physics.gravity * T / 2f;
+
+            return velocity;
+        }
+		
+		private Vector3 GetProjectil2Velocity(Vector3 target, Vector3 origin)
+        {
+            const float projectileSpeed = 7.5f;
+
+            Vector3 velocity = Vector3.zero;
+            Vector3 toTarget = target - origin;
+
+            float gSquared = Physics.gravity.sqrMagnitude;
+            float b = projectileSpeed * projectileSpeed + Vector3.Dot(toTarget, Physics.gravity);
+            float discriminant = b * b - gSquared * toTarget.sqrMagnitude;
+
+           /* // Check whether the target is reachable at max speed or less.
+            if (discriminant < 0)
+            {
+                velocity = toTarget;
+                velocity.y = 0;
+                velocity.Normalize();
+                velocity.y = 0.7f;
+
+                velocity *= projectileSpeed;
+                return velocity;
+            }*/
+
+            float discRoot = Mathf.Sqrt(discriminant);
+
+            // Highest
+            float T_max = Mathf.Sqrt((b + discRoot) * 2f / gSquared);
+
+            // Lowest speed arc
+            float T_lowEnergy = Mathf.Sqrt(Mathf.Sqrt(toTarget.sqrMagnitude * 4f / gSquared));
+
+            // Most direct with max speed
+            float T_min = Mathf.Sqrt((b - discRoot) * 2f / gSquared);
+
+            float T = 0;
+
+            // 0 = highest, 1 = lowest, 2 = most direct
+            int shotType = 0;
+
+            switch (shotType)
+            {
+                case 0:
+                    T = T_max;
+                    break;
+                case 1:
+                    T = T_lowEnergy;
+                    break;
+                case 2:
+                    T = T_min;
+                    break;
+                default:
+                    break;
+            }
+
+            velocity = toTarget / T - Physics.gravity * T / 2f;
+
+            return velocity;
+        }
+		
+		private Vector3 GetProjectil3Velocity(Vector3 target, Vector3 origin)
+        {
+            const float projectileSpeed = 7.5f;
+
+            Vector3 velocity = Vector3.zero;
+            Vector3 toTarget = target - origin;
+
+            float gSquared = Physics.gravity.sqrMagnitude;
+            float b = projectileSpeed * projectileSpeed + Vector3.Dot(toTarget, Physics.gravity);
+            float discriminant = b * b - gSquared * toTarget.sqrMagnitude;
+
+            /*// Check whether the target is reachable at max speed or less.
+            if (discriminant < 0)
+            {
+                velocity = toTarget;
+                velocity.y = 0;
+                velocity.Normalize();
+                velocity.y = 0.7f;
+
+                velocity *= projectileSpeed;
+                return velocity;
+            }*/
+
+            float discRoot = Mathf.Sqrt(discriminant);
+
+            // Highest
+            float T_max = Mathf.Sqrt((b + discRoot) * 2f / gSquared);
+
+            // Lowest speed arc
+            float T_lowEnergy = Mathf.Sqrt(Mathf.Sqrt(toTarget.sqrMagnitude * 4f / gSquared));
+
+            // Most direct with max speed
+            float T_min = Mathf.Sqrt((b - discRoot) * 2f / gSquared);
+
+            float T = 0;
+
+            // 0 = highest, 1 = lowest, 2 = most direct
+            int shotType = 2;
+
+            switch (shotType)
+            {
+                case 0:
+                    T = T_max;
+                    break;
+                case 1:
+                    T = T_lowEnergy;
+                    break;
+                case 2:
+                    T = T_min;
+                    break;
+                default:
+                    break;
+            }
+
+            velocity = toTarget / T - Physics.gravity * T / 2f;
+
+            return velocity;
+        }
+    }
+}
---
+++ Assets/2DGamekit/Scripts/Character/MonoBehaviours/HomemPeixe.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+
+public class HomemPeixe : Ally
+{
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (enemy != null) 
+		{
+			if (isDashing)
+			{
+				m_Rigidbody2D.velocity = new Vector2(transform.localScale.x * m_DashForce, 0);
+			}
+			else if (!isHitted)
+			{
+				distToPlayer = enemy.transform.position.x - transform.position.x;
+				distToPlayerY = enemy.transform.position.y - transform.position.y;
+
+				if (Mathf.Abs(distToPlayer) < 0.25f)
+				{
+					GetComponent<Rigidbody2D>().velocity = new Vector2(0f, m_Rigidbody2D.velocity.y);
+					anim.SetBool("IsWaiting", true);
+				}
+				else if (Mathf.Abs(distToPlayer) > 0.25f && Mathf.Abs(distToPlayer) < meleeDist && Mathf.Abs(distToPlayerY) < 2f)
+				{
+					GetComponent<Rigidbody2D>().velocity = new Vector2(0f, m_Rigidbody2D.velocity.y);
+					if ((distToPlayer > 0f && transform.localScale.x < 0f) || (distToPlayer < 0f && transform.localScale.x > 0f)) 
+						Flip();
+					if (canAttack)
+					{
+						MeleeAttack();
+					}
+				}
+				else if (Mathf.Abs(distToPlayer) > meleeDist && Mathf.Abs(distToPlayer) < rangeDist)
+				{
+					anim.SetBool("IsWaiting", false);
+					m_Rigidbody2D.velocity = new Vector2(distToPlayer / Mathf.Abs(distToPlayer) * speed, m_Rigidbody2D.velocity.y);
+				}
+				else
+				{
+					if (!endDecision)
+					{
+						if ((distToPlayer > 0f && transform.localScale.x < 0f) || (distToPlayer < 0f && transform.localScale.x > 0f)) 
+							Flip();
+
+						/*if (randomDecision < 0.4f)
+							Run();
+						else if (randomDecision >= 0.4f && randomDecision < 0.6f)
+							Jump();
+						else if (randomDecision >= 0.6f && randomDecision < 0.8f)
+							StartCoroutine(Dash());
+						else if (randomDecision >= 0.8f && randomDecision < 0.95f)
+							RangeAttack();
+						else
+							Idle();*/
+							
+						if (randomDecision < 0.95f)
+							Jump();
+						else
+							Idle();
+					}
+					else
+					{
+						endDecision = false;
+					}
+				}
+			}
+			else if (isHitted)
+			{
+				if ((distToPlayer > 0f && transform.localScale.x > 0f) || (distToPlayer < 0f && transform.localScale.x < 0f))
+				{
+					Flip();
+					StartCoroutine(Dash());
+				}
+				else
+					StartCoroutine(Dash());
+			}
+		}
+		else 
+		{
+			enemy = GameObject.FindWithTag("Player");
+		}
+
+		if (transform.localScale.x * m_Rigidbody2D.velocity.x > 0 && !m_FacingRight && life > 0)
+		{
+			// ... flip the player.
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (transform.localScale.x * m_Rigidbody2D.velocity.x < 0 && m_FacingRight && life > 0)
+		{
+			// ... flip the player.
+			Flip();
+		}
+	}
+
+	public new void Jump()
+	{
+		            /*    m_MoveVector = new Vector2(0, 20);
+
+		Vector3 targetVelocity = new Vector2(distToPlayer / Mathf.Abs(distToPlayer) * speed, m_Rigidbody2D.velocity.y);
+		Vector3 velocity = Vector3.zero;
+		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, 0.05f);*/
+		if (doOnceDecision)
+		{
+			anim.SetBool("IsWaiting", false);
+			m_Rigidbody2D.AddForce(new Vector2(0f, 850f));
+			StartCoroutine(NextDecision(1f));
+		}
+	}
+}
